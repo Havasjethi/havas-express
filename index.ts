d9e41de40069c70b361c7e5f18e3ab5c91368b68
Jozsef Kulcsar <jozsef.kulcsar@rabit.hu>
2021-03-28 15:55:20 +0200

    Add export statements

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { Get, Method, Post } from "./src/decorators/method_decorator";
 import { App } from "./src/app";
 import { Router } from "./src/router";
+import { Host, Path } from "./src/decorators/class_decorator";
 
 export {
   Get, Post, Method,
-  App, Router
+  App, Router,
+  Path, Host
 };

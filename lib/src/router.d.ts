e1bf5632751ae6f0948a70ebf9d8f6ecc28ba294
Jozsef Kulcsar <havasjethi@gmail.com>
2021-05-28 17:23:10 +0200

    Remove files from version controlling

--- lib/src/router.d.ts
+++
@@ -1,6 +0,0 @@
-import { Router as ExpressRouter } from "express";
-import { Routable } from "./classes/routable";
-export declare class Router extends Routable<ExpressRouter> {
-    constructor();
-    remove_layers(): void;
-}
